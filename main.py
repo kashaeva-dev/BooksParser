cc8084d8df22a91979005a6e90891681730ee4a2
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-07-05 18:31:47 +0300

    Download book covers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from contextlib import suppress
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 from bs4 import BeautifulSoup
 
@@ -11,6 +12,18 @@ def get_book(book_id):
     url = "https://tululu.org/txt.php"
 
     payload = {'id': book_id}
+
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+
+    check_for_redirect(response)
+
+    return response.text
+
+
+def get_book(url):
+
+
 
     response = requests.get(url, params=payload)
     response.raise_for_status()
@@ -34,6 +47,24 @@ def download_txt(id, filename, folder):
         file.write(book)
 
 
+def download_image(url, folder):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    check_for_redirect(response)
+
+    _, image_filename = os.path.split(urlsplit(url).path)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    image_path = os.path.join(folder, image_filename)
+
+
+    with open(image_path, 'wb') as file:
+        file.write(response.content)
+
+
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
@@ -49,9 +80,11 @@ def get_book_details(id):
     soup = BeautifulSoup(response.text, 'lxml')
 
     book_details = soup.title.text.split(', ')[0]
-    name, author = book_details.split(' - ')
+    name, _ = book_details.split(' - ')
+    book_image_src = soup.find('div', class_='bookimage').find('img')['src']
+    book_image_url = urljoin('https://tululu.org/', book_image_src)
 
-    return name, author
+    return name, book_image_url
 
 
 def main():
@@ -62,4 +95,7 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    for id in range(1, 11):
+        with suppress(requests.HTTPError):
+            _, book_image_url = get_book_details(id)
+            download_image(book_image_url, 'images')

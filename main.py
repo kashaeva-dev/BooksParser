2651fc8e120d82d856cc30744c3485ef84239654
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-07-05 15:45:52 +0300

    Alter function to download txt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import os
 from contextlib import suppress
+from pathvalidate import sanitize_filename
+
+from bs4 import BeautifulSoup
 
 
 def get_book(book_id):
@@ -17,12 +20,14 @@ def get_book(book_id):
     return response.text
 
 
-def save_book(filename, book):
-    if not os.path.exists('books'):
-        os.makedirs('books')
+def download_txt(id, filename, folder):
+    book = get_book(id)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
+    filename = sanitize_filename(filename)
     book_file = ".".join([filename, 'txt'])
-    book_path = "/".join(['books', book_file])
+    book_path = os.path.join(folder, book_file)
 
 
     with open(book_path, 'w') as file:
@@ -34,11 +39,27 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+def get_book_details(id):
+    url = f'https://tululu.org/b{id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    check_for_redirect(response)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    book_details = soup.title.text.split(', ')[0]
+    name, author = book_details.split(' - ')
+
+    return name, author
+
+
 def main():
     for id in range(1, 11):
         with suppress(requests.HTTPError):
-            book = get_book(id)
-            save_book(str(id), book)
+            print(id)
+            filename, _ = get_book_details(id)
+            download_txt(id, filename, 'books')
 
 
 if __name__=="__main__":
